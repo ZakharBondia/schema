429a564c256542577cee8fa6d8144b11f9efb745
Zakhar <Z-1st@ukr.net>
2019-10-06 22:47:18 +0300

    fix

--- schm.h
+++ schm.h
@@ -1,42 +1,137 @@
-import qbs
+#pragma once
+#include <cstring>
+#include <type_traits>
+#include <array>
 
-CppApplication {
-    qbs.targetPlatform: "macos"
+//-------------------------
+namespace schm {
 
-    cpp.compilerIncludePaths: [
-    "/usr/local/include/",
-    ]
-    cpp.compilerFrameworkPaths: ""
-    cpp.compilerLibraryPaths: ""
+template<class To, class From>
+auto bit_cast(const From &src) noexcept
+    -> std::enable_if_t<(sizeof(To) == sizeof(From)) && std::is_trivially_copyable<From>::value
+                            && std::is_trivial<To>::value,
+                        To>
+{
+    To dst;
+    std::memcpy(&dst, &src, sizeof(To));
+    return dst;
+}
+//-------------------------
+template<class T>
+constexpr T *null = nullptr;
+//-------------------------
+// Former F;
+// Schema S
+// Member M;
+//-------------------------
 
-    cpp.cxxFlags: ["-fconcepts", "-Wno-pointer-arith",]
+template<class M>
+using type = typename M::type;
 
-    consoleApplication: true
-    files: [
-        "src/**/*.cpp",
-        "src/**/*.h",
-    ]
-    cpp.includePaths: [
-        "./dep",
-        "./src",
+template<class M>
+using parent = typename M::parent;
 
-        "/usr/local/include/",
-
-    ]
-
-    Group {
-        name: "dep"
-        files:  "dep/**"
-    }
-
-    cpp.dsymutilPath: "/usr/local/Cellar/llvm/8.0.1/bin/dsymutil"
-    cpp.cxxLanguageVersion: "c++14"
-    cpp.minimumMacosVersion: "10.14"
-
-    Group {     // Properties for the produced executable
-        fileTagsFilter: "application"
-        qbs.install: true
-        qbs.installDir: "bin"
-    }
+template<class M>
+constexpr auto access(parent<M> &p) -> type<M> &
+{
+    return M::access(p);
 }
 
+template<class M>
+constexpr auto access(const parent<M> &p) -> const type<M> &
+{
+    return M::access(p);
+}
+
+template<class M>
+constexpr auto name() -> const char *
+{
+    return M::name;
+}
+
+//-------------------------
+struct as_bool
+{
+    template<class M>
+    using type = bool;
+};
+
+struct as_is
+{
+    template<class M>
+    using type = type<M>;
+};
+
+//-------------------------
+template<template<class F> class SC>
+constexpr std::size_t get_member_number(SC<as_is> * = nullptr)
+{
+    return sizeof(SC<as_bool>);
+}
+
+template<class S>
+constexpr std::size_t member_number = get_member_number(null<S>);
+
+//-------------------------
+template<class S, class T>
+using members = std::array<T, member_number<S>>;
+
+
+template<class S, class F>
+struct reformator
+{
+    template<template<class> class SC>
+    static auto test(SC<as_is> * = nullptr) -> SC<F>;
+    using type = decltype(test(null<S>));
+};
+
+template<class S, class F>
+using reform = typename reformator<S, F>::type;
+
+//-------------------------
+template<class R, class F>
+struct make_transfomer
+{
+    struct former
+    {
+        template<class M>
+        struct type
+        {
+            R v{F{}(M{})};
+        };
+    };
+};
+template<class T>
+using former = typename T::former;
+
+template<class S, class R, class F>
+auto tranform(F /*f*/) -> members<S, R>
+{
+    return bit_cast<members<S, R>>(reform<S, former<make_transfomer<R, F>>>{});
+}
+//-------------------------
+
+} // namespace schm
+
+#define SCHM_REF(TYPE, NAME) \
+struct _##NAME \
+{ \
+    using parent = _self; \
+    using type = TYPE; \
+    static constexpr const char *name = #NAME; \
+    static type &access(parent& p) { return p.NAME; } \
+    static type const& access(parent const& p) { return p.NAME; } \
+}; \
+
+//-------------------------
+#define SCHM_BEG(NAME) \
+    template<class F> \
+    struct _##NAME; \
+\
+    using NAME = _##NAME<schm::as_is>; \
+\
+    template<class F> \
+    struct _##NAME{ using _self = _##NAME;\
+
+#define SCHM_MEM(T, NAME) SCHM_REF(T, NAME) typename F:: template type<_##NAME> NAME;
+#define SCHM_END };
